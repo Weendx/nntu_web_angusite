e1ddaa1ff9faa5b36288bf5fdde4c13a85a55968
Weendx <muha10bin@list.ru>
2023-12-02 21:02:53 +0300

    add: create page-layout component

---
+++ src/app/shared/components/page-layout/page-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-page-layout',
+  templateUrl: './page-layout.component.html',
+  styleUrls: ['./page-layout.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PageLayoutComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './components/container/container.component';
+import { PageLayoutComponent } from './components/page-layout/page-layout.component';
 
 
 
 @NgModule({
   declarations: [
-    ContainerComponent
+    ContainerComponent,
+    PageLayoutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ContainerComponent
+    ContainerComponent,
+    PageLayoutComponent
   ]
 })
 export class SharedModule { }
