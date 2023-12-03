7ef5e6db7ea73f7a7f7b18806e8e37ca6ddf2a4c
Weendx <muha10bin@list.ru>
2023-12-03 08:52:18 +0300

    refactor: move registration-form to AuthModule

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { RegistrationFormComponent } from 'src/app/components/registration-form/registration-form.component';
+import { AuthModule } from 'src/app/shared/modules/auth/auth.module';
 
 
 const routes: Routes = [
@@ -12,12 +12,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    SignupComponent,
-    RegistrationFormComponent
+    SignupComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    AuthModule,
     RouterModule.forChild(routes)
   ],
   exports: [
