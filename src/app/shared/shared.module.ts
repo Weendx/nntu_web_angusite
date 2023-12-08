9a3d9f70d47e533bf47757500eee5e867e56cf02
Weendx <muha10bin@list.ru>
2023-12-08 19:29:20 +0300

    refactor: extract monitoring to component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './components/container/container.component';
 import { PageLayoutComponent } from './components/page-layout/page-layout.component';
+import { MonitoringComponent } from './components/monitoring/monitoring.component';
 
 
 
 @NgModule({
   declarations: [
     ContainerComponent,
-    PageLayoutComponent
+    PageLayoutComponent,
+    MonitoringComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     ContainerComponent,
-    PageLayoutComponent
+    PageLayoutComponent,
+    MonitoringComponent
   ]
 })
 export class SharedModule { }
