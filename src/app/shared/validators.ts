cf5902329129cf5ce117d3799432cb24fa79c16a
Weendx <muha10bin@list.ru>
2023-12-08 16:12:51 +0300

    add: valueExistsValidator

--- src/app/shared/validators.ts
+++ src/app/shared/validators.ts
@@ -1,4 +1,5 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, AsyncValidatorFn, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { Observable, map } from "rxjs";
 
 export function equalToValidator(secondControlName: string): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
@@ -6,5 +7,17 @@ export function equalToValidator(secondControlName: string): ValidatorFn {
     if (!second)
       return null;
     return second.value === control.value ? null : { equalTo: true }
+  }
+}
+
+export interface valueChecker {
+  (value: any): Observable<boolean>
+}
+
+export function valueExistsValidator(checker: valueChecker): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    return checker(control.value).pipe(
+      map( isExists => isExists ? { valueExists: true } : null )
+    );
   }
 }
