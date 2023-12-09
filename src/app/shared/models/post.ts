d01c1fcf10a095e6c44ad52f87a7cec0ea47f599
Weendx <muha10bin@list.ru>
2023-12-09 20:38:46 +0300

    update: post model

--- src/app/shared/models/post.ts
+++ src/app/shared/models/post.ts
@@ -2,6 +2,8 @@ export interface IPost {
     id?: number,
     title: string,
     body: string,
+    bodyPreview: string,
     timestamp: number,
-    userId: number
+    userId: number,
+    views: number
 }
