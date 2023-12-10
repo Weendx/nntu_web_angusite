c6b675bc6dd9cd9e30560ff0845cf84a9bbcac7e
Weendx <muha10bin@list.ru>
2023-12-10 22:27:25 +0300

    add: dynamic post loading on news page

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IPost } from 'src/app/shared/models/post';
+import { PostService } from 'src/app/shared/services/post.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
   public newsFilter = '';
+  public posts!: IPost[];
+
+  constructor( public postService: PostService, private router: Router ) {}
+
+  public ngOnInit(): void {
+    this.postService.getAll().subscribe((posts) => this.posts = posts);
+  }
 
   public clearFilter() {
     this.newsFilter = '';
+  }
+
+  public toPost(postId: number) {
+    this.router.navigate([`/post/${postId}`]);
   }
 }
