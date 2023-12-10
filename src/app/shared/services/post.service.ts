2e7c2a3fe80c7d36c420c87b0033587b69a41bfc
Weendx <muha10bin@list.ru>
2023-12-10 20:37:43 +0300

    add: post.service - connection errors handling

--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPost } from '../models/post';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,33 @@ export class PostService {
 
   private url = 'http://localhost:3000';
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient, private notifyService: NotificationService ) {}
 
   public getAll(): Observable<IPost[]> {
-    return this.http.get<IPost[]>(this.url+'/posts?_sort=timestamp&_order=desc');
+    return this.http.get<IPost[]>(this.url+'/posts?_sort=timestamp&_order=desc').pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.notifyService.send('[Post] Проблема с подключением к серверу. ' + error.message);
+        return throwError(() => error.message);
+      })
+    );
   }
 
   public getById(id: number): Observable<IPost> {
-    return this.http.get<IPost>(this.url+`/posts/${id}`);
+    return this.http.get<IPost>(this.url+`/posts/${id}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.notifyService.send('[Post] Проблема с подключением к серверу. ' + error.message);
+        return throwError(() => error.message);
+      })
+    );
   }
 
   public create(post: IPost): Observable<IPost> {
-    return this.http.post<IPost>(this.url+'/posts', post);
+    return this.http.post<IPost>(this.url+'/posts', post).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.notifyService.send('[Post] Проблема с подключением к серверу. ' + error.message);
+        return throwError(() => error.message);
+      })
+    );
   }
 
 }
