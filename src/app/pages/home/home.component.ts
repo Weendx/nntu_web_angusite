6a8a972c655f2a03c83cdb93920abeeda123345d
Weendx <muha10bin@list.ru>
2023-12-19 13:27:14 +0300

    add: dynamic posts loading to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IPost } from 'src/app/shared/models';
+import { PostService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  public posts: IPost[] = [];
+
+  constructor( public postService: PostService, private router: Router ) {}
+
+  public ngOnInit(): void {
+    this.postService.getAll().subscribe((posts) => this.posts = posts);
+  }
+
+  public toPost(postId: number) {
+    this.router.navigate([`/post/${postId}`]);
+  }
 
 }
