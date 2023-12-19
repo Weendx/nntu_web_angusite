545d3fd60df6d20f9328e53e20344a4f4428ee2b
Weendx <muha10bin@list.ru>
2023-12-19 13:09:04 +0300

    add: post filtering

--- src/app/shared/pipes/filter-posts.pipe.ts
+++ src/app/shared/pipes/filter-posts.pipe.ts
@@ -8,7 +8,11 @@ export class FilterPostsPipe implements PipeTransform {
 
   transform(posts: IPost[], search: string): IPost[] {
     if (search.length === 0) return posts;
-    return posts.filter(p => p.body.toLowerCase().includes(search.toLowerCase()));
+    return posts.filter(p => 
+      p.body.toLowerCase().includes(search.toLowerCase()) 
+      || p.title.toLowerCase().includes(search.toLowerCase())
+      || p.bodyPreview.toLowerCase().includes(search.toLowerCase())
+    );
   }
 
 }
